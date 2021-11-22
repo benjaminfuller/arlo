11c043744781754f79d36f9d8d3e543a9d59af41
root <VoTeR Center>
2021-11-22 16:54:49 -0500

    no longer running noauth from arlo startup script

--- run-dev.sh
+++ run-dev.sh
@@ -5,7 +5,7 @@
 export FLASK_ENV=${FLASK_ENV:-development}
 trap 'kill 0' SIGINT SIGHUP
 cd "$(dirname "${BASH_SOURCE[0]}")"
-PORT=8080 poetry run python -m noauth &
+#PORT=8080 poetry run python -m noauth &
 poetry run python -m server.main &
 poetry run python -m server.worker.worker &
 yarn --cwd client start
